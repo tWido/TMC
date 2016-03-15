a4e94e4c9ef8e564eb073ee26e698987a5ec1fe4
Wido <tomas.wido@gmail.com>
2016-03-15 21:00:16 +0100

    Continuing api

--- src/api_calls.hpp
+++ src/api_calls.hpp
@@ -19,6 +19,10 @@ uint8_t DefaultChanel(){
     return 0x01;
 }
 
+uint8_t DeafultRate(){
+    return 1;
+}
+
 int CheckDeviceAbility(){
     //not implementeed
     return-1;}
@@ -37,13 +41,13 @@ int SendMessage(Message &message, FT_HANDLE &handle){
     return -1;
 }
 
-int CheckIcomingQueue(){
+int CheckIncomingQueue(){
     //not implemented
     //check if queue is empty, process possible messages
     return -1;
 }
 
-int GetResponseMess(uint16_t last_msg, uint16_t expected_msg, uint8_t *mess ){
+int GetResponseMess(uint16_t last_msg_sent, uint16_t expected_msg, uint8_t *mess ){
     //not implemented
     //check incoming queue for error messages, return fail on error, 
     // other messages handled
@@ -52,37 +56,61 @@ int GetResponseMess(uint16_t last_msg, uint16_t expected_msg, uint8_t *mess ){
 }
 
 int Identify( FT_HANDLE &handle, uint8_t dest = DefaultDest(), uint8_t source = DefaultSource() ){
-    CheckIcomingQueue();
+    CheckIncomingQueue();
     IdentifyMs mes(dest, source);
     SendMessage(mes, handle); 
-    CheckIcomingQueue();
+    CheckIncomingQueue();
     return 0;
 }
 
 int EnableChannel(FT_HANDLE &handle, uint8_t chanel = DefaultChanel(), uint8_t dest = DefaultDest(), uint8_t source = DefaultSource()){   
-    CheckIcomingQueue();
+    CheckIncomingQueue();
     SetChannelState mes(chanel, 1, dest, source);
     SendMessage(mes, handle);
-    CheckIcomingQueue();
+    CheckIncomingQueue();
     return 0;
 }
 
 int DisableChannel(FT_HANDLE &handle,uint8_t chanel = DefaultChanel(), uint8_t dest = DefaultDest(), uint8_t source = DefaultSource()){  
-    CheckIcomingQueue();
+    CheckIncomingQueue();
     SetChannelState mes(chanel, 2, dest, source);
     SendMessage(mes, handle);
-    CheckIcomingQueue();
+    CheckIncomingQueue();
     return 0;
 }
 
 int ChannelState(FT_HANDLE &handle, GetChannelState *info, uint8_t chanel = DefaultChanel(), uint8_t dest = DefaultDest(), uint8_t source = DefaultSource()){ 
-    CheckIcomingQueue();
+    CheckIncomingQueue();
     ReqChannelState mes(chanel, dest, source);
     SendMessage(mes, handle);
     uint8_t *ret;
     if (GetResponseMess(REQ_CHANENABLESTATE, GET_CHANENABLESTATE, ret) != 0) return -1;
     info = new GetChannelState(ret);
     return 0;
+}
+
+int DisconnectHW(FT_HANDLE &handle, uint8_t dest = DefaultDest(), uint8_t source = DefaultSource()){
+    CheckIncomingQueue();
+    HwDisconnect mes(dest,source);
+    SendMessage(mes, handle);
+    CheckIncomingQueue();
+    return 0;
+}
+
+int StartUpdateMess(FT_HANDLE &handle, uint8_t rate = DeafultRate(), uint8_t dest = DefaultDest(), uint8_t source = DefaultSource()){
+    CheckIncomingQueue();
+    StartUpdateMessages mes(dest,source);
+    if (mes.SetUpdaterate(rate) == IGNORED_PARAM) printf("This parameter is ignored in connected device. Using default.\n");
+    SendMessage(mes, handle);
+    CheckIncomingQueue();
+    return 0;
+}
+
+int StopUpdateMess(FT_HANDLE &handle, uint8_t dest = DefaultDest(), uint8_t source = DefaultSource()){
+    CheckIncomingQueue();
+    StopUpdateMessages mes(dest,source);
+    SendMessage(mes, handle);
+    CheckIncomingQueue();
 }
 
 #endif 
