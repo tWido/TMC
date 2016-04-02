e28289588a6633e6928ab0f7270a489a4e5a5308
Wido <tomas.wido@gmail.com>
2016-04-02 17:37:51 +0200

    Layout for calls

--- src/cmd.hpp
+++ src/cmd.hpp
@@ -2,10 +2,31 @@
 #define	CMD_HPP
 #include "api_calls.hpp"
 #include "device.hpp"
+#include <map>
+#include <vector>
+
+
+typedef int (*helper)(std::vector<string>);
+typedef std::map<std::string,helper> call_map;
+
+int IdentC(std::vector<string> args){
+    //not implemented
+    return 0;
+}
+
+int EnableChanC(std::vector<string> args){
+    //not implemented
+    return 0;
+}
+
+call_map calls = {
+    std::make_pair("Flash", &IdentC),
+    std::make_pair("ChanEn", &EnableChanC)
+};
 
 
 int run_cmd(){
-    device_calls::Identify(opened_device.handle);
+   
     return 0;
 }
 
