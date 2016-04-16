a955a5f48babcb49dab8a00cf186c9c155437592
Wido <tomas.wido@gmail.com>
2016-04-16 20:38:35 +0200

    Added arguments parsing

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,6 @@
 using namespace std;
 
 int UI = 1; // 1 -> cmd, 2-> graphical, 3 - > validate file
-
-int ParseCmdArgs(int argc, char** argv){
-    return 0;
-}
         
 void FailExit(int ret_code){
 
@@ -46,8 +42,30 @@ switch (ret_code){
 }
 
 
-int main(int argc, char** argv) {
-    ParseCmdArgs(argc, argv);
+int main(int argc, char** argv) {   
+    for (int i = 1; i < argc; i++){
+        if (strcmp(argv[i],"-h") == 0 || strcmp(argv[i],"--help") == 0) {
+            printf("Options\n");
+            printf("  -h, --help                    prints this help message and exit\n");
+            printf("  -c, --command-line            starts with command line interface, default\n");
+            printf("  -g, --graphical               after initialization starts graphical interface\n");
+            printf("  -v, --validate                validate syntax of commands; use with \"< FILE\" to validate control file\n");
+            return 0;
+        }
+        else if (strcmp(argv[i],"-c") == 0 || strcmp(argv[i],"--command-line") == 0) UI=1;
+        else if (strcmp(argv[i],"-g") == 0 || strcmp(argv[i],"--graphical") == 0) UI=2;
+        else if (strcmp(argv[i],"-v") == 0 || strcmp(argv[i],"--validate-file") == 0) UI=3;
+        else {
+            printf("Invalid option: %s\n", argv[i]);
+            printf("Options\n");
+            printf("  -h, --help                    prints this help message and exit\n");
+            printf("  -c, --command-line            starts with command line interface, default\n");
+            printf("  -g, --graphical               after initialization starts graphical interface\n");
+            printf("  -v, --validate                validate syntax of commands; use with \"< FILE\" to validate control file \n");
+            return 0;
+        }
+    }
+    
     int ret_code = 0;
     if (UI !=3) ret_code = init();
     if ( ret_code != 0 ){ 
@@ -57,7 +75,7 @@ int main(int argc, char** argv) {
     
     if (UI == 1) ret_code = run_cmd(1);
     if (UI == 3) ret_code = run_cmd(3);
-    
+
     if ( ret_code != 0 ) {
         FailExit(ret_code);
         return ret_code;
