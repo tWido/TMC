acf4cea107c51d1efa67702c9e00edb16c872a5f
Wido <tomas.wido@gmail.com>
2016-04-19 11:06:21 +0200

    Added parts of basic layout

--- src/gui.hpp
+++ src/gui.hpp
@@ -17,6 +17,12 @@
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QPushButton>
+#include <QPixmap>
+#include <QIcon>
+
+#include "device.hpp"
 
 class GUI: public QWidget{
     public:
@@ -26,17 +32,30 @@ class GUI: public QWidget{
         QMenu *device_menu;
         QMenu *help_menu;
         QLabel *dev_name;
+        QLabel *dev_ser;
         QLabel *serial;
         QAction *device;
         QAction *device2;
+        QRadioButton *chan_1;
+        QRadioButton *chan_2;
+        QRadioButton *chan_3;
+        QPushButton *flash_button;
+        QPushButton *home_button;
+        QPushButton *forward;
+        QPushButton *backward;
+        QLabel *moves;
+        QRadioButton *relm;
+        QRadioButton *absm;
+        QRadioButton *jogm;
+        QRadioButton *velm;
 
         void Setup(){
+            //main
             main = new QMainWindow(this);
             main->resize(800, 600);
             main ->setWindowTitle("Thorlabs motor controller");
-            dev_name = new QLabel(main);
-            dev_name->setText("waaat");
-            dev_name->setGeometry(50,150,100,20);
+            
+            //Menu
             menuBar = new QMenuBar(main);
             menuBar->setGeometry(0, 0, 800, 40);
             menuBar->move(0,0);
@@ -50,6 +69,70 @@ class GUI: public QWidget{
             menuBar->addMenu(control_menu);
             menuBar->addMenu(device_menu);
             menuBar->addMenu(help_menu);
+            
+            //Device labels
+            dev_name = new QLabel(main);
+            std::string dname = "Device: ";
+            QFont font = dev_name->font();
+            font.setPointSize(14);
+            dev_name->setFont(font);
+            dname.append(opened_device.dev_type_name);
+            dev_name->setText(dname.c_str());
+            dev_name->setGeometry(20,40,200,30);
+            dev_ser = new QLabel(main);
+            font = dev_ser->font();
+            font.setPointSize(14);
+            dev_ser->setFont(font);
+            std::string serial = "SN: ";
+            serial.append(opened_device.SN);
+            dev_ser->setGeometry(20,65,200,30);
+            dev_ser->setText(serial.c_str());
+            
+            //channel switch
+            font.setPointSize(12);
+            chan_1 = new QRadioButton(main);
+            chan_1->setGeometry(30,200,80,50);
+            chan_1->setText("Chan 1");
+            chan_1->setChecked(true);
+            chan_1->setFont(font);
+            if (opened_device.channels == 2 || (opened_device.bays >=2 && opened_device.bay_used[1])){
+                chan_2 = new QRadioButton(main);
+                chan_2->setGeometry(120,200,80,50);
+                chan_2->setText("Chan 2");
+                chan_2->setFont(font);
+                chan_1->setChecked(false);
+            }
+            if (opened_device.bays == 3 && opened_device.bay_used[2]){
+                chan_3 = new QRadioButton(main);
+                chan_3->setGeometry(210,200,80,50);
+                chan_3->setText("Chan 3");
+                chan_3->setFont(font);
+                chan_1->setChecked(false);
+            }    
+
+            //flash and home buttons
+            flash_button = new QPushButton(main);
+            flash_button->setGeometry(50,300,100,50);
+            flash_button->setText("Flash LED");
+            home_button = new QPushButton(main);
+            home_button->setGeometry(200,300,100,50);
+            home_button->setText("Home");
+            
+            //up&down buttons
+            forward = new QPushButton(main);
+            forward->setIcon(QIcon("./src/triangle_up.png"));
+            forward->setIconSize(QSize(65,65));
+            forward->setGeometry(400,100,70,70);
+            backward = new QPushButton(main);
+            backward->setIcon(QIcon("./src/triangle_down.png"));
+            backward->setIconSize(QSize(65,65));
+            backward->setGeometry(400,180,70,70);
+            
+            //moves
+            
+            
+            
+            //status bar
             
             main->show();
         }
