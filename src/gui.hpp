53d99b0eb6fe70b67cf8de6c1d0a173de1e7a8a2
Wido <tomas.wido@gmail.com>
2016-04-20 16:57:15 +0200

    Refactor cause im retard

--- src/gui.hpp
+++ src/gui.hpp
@@ -29,11 +29,30 @@
 
 #include "device.hpp"
 
+class DevOpt: public QWidget{
+    public:    
+        void Setup(){
+            this->show();
+        } 
+
+};
+
+class MovOpt: public QWidget{
+    public:    
+        void Setup(){
+            this->show();
+        }
+
+};
+
 class GUI: public QMainWindow{
     
     public slots:
         void openDevOptions(){};
-        void openMoveOptions(){};
+        void openMoveOptions(){
+            moveOpt = new MovOpt();
+            moveOpt->Setup();
+        };
         void openHelp(){};
         void openDoc(){};
         void switchDev(int index){};
@@ -47,7 +66,6 @@ class GUI: public QMainWindow{
     
     public:
         int actual_channel;
-        QMainWindow *main;
         QMenuBar *menuBar;
         QMenu *control_menu;
         QMenu *device_menu;
@@ -100,29 +118,30 @@ class GUI: public QMainWindow{
         QLabel *position;
         QLabel *velocity;
         QLabel *enc_count;
+        MovOpt *moveOpt;
+        DevOpt *devOpt;
                     
         void Setup(){
             //main
-            main = new QMainWindow(this);
-            main->resize(800, 600);
-            main ->setWindowTitle("Thorlabs motor controller");
-            
+            this->resize(800, 600);
+            this ->setWindowTitle("Thorlabs motor controller");
+
             //Menu
-            menuBar = new QMenuBar(main);
+            menuBar = new QMenuBar(this);
             menuBar->setGeometry(0, 0, 800, 40);
             menuBar->move(0,0);
-            main->setMenuBar(menuBar);
+            this->setMenuBar(menuBar);
             control_menu = new QMenu("Control",menuBar);
             device_menu = new QMenu("Device",menuBar);
             help_menu = new QMenu(menuBar);
             help_menu->setTitle("Help");
-            exit = new QMenu("Exit",main);
-            exit_action = new QAction("Exit",main);
+            exit = new QMenu("Exit",this);
+            exit_action = new QAction("Exit",this);
             exit->addAction(exit_action);
             connect(exit_action, &QAction::triggered, this, &GUI::quit);
             menuBar->addMenu(control_menu);
-            device_opt_action = new QAction("Device settings", main);
-            move_opt_action = new QAction("Move options", main);
+            device_opt_action = new QAction("Device settings", this);
+            move_opt_action = new QAction("Move options", this);
             control_menu->addAction(device_opt_action);
             control_menu->addAction(move_opt_action);
             connect(device_opt_action, &QAction::triggered, this, &GUI::openDevOptions);
@@ -133,12 +152,12 @@ class GUI: public QMainWindow{
                 std::string dev_label = std::to_string(i+1);
                 dev_label.append(": ");
                 dev_label.append(connected_device->SN);
-                device_switch_actions[i] = new QAction(dev_label.c_str(),main);
+                device_switch_actions[i] = new QAction(dev_label.c_str(),this);
                 connect(move_opt_action, &QAction::triggered, this, [this](int i){ switchDev(i); });
             }
             menuBar->addMenu(help_menu);
-            help_action = new QAction("Help",main);
-            doc_action = new QAction("Documentation",main);
+            help_action = new QAction("Help",this);
+            doc_action = new QAction("Documentation",this);
             help_menu->addAction(help_action);
             help_menu->addAction(doc_action);
             connect(help_action, &QAction::triggered, this, &GUI::openHelp);
@@ -146,7 +165,7 @@ class GUI: public QMainWindow{
             menuBar->addMenu(exit);
                       
             //Device labels
-            dev_name = new QLabel(main);
+            dev_name = new QLabel(this);
             std::string dname = "Device: ";
             QFont font = dev_name->font();
             font.setPointSize(14);
@@ -154,7 +173,7 @@ class GUI: public QMainWindow{
             dname.append(opened_device.dev_type_name);
             dev_name->setText(dname.c_str());
             dev_name->setGeometry(20,40,250,30);
-            dev_ser = new QLabel(main);
+            dev_ser = new QLabel(this);
             font = dev_ser->font();
             font.setPointSize(14);
             dev_ser->setFont(font);
@@ -164,17 +183,17 @@ class GUI: public QMainWindow{
             dev_ser->setText(serial.c_str());
             
             //channel switch
-            channels = new QGroupBox("Channels",main);
+            channels = new QGroupBox("Channels",this);
             chan_box = new QVBoxLayout();
             font.setPointSize(12);
             channels->setFont(font);
-            chan_1 = new QRadioButton(main);
+            chan_1 = new QRadioButton(this);
             chan_1->resize(100,50);
             chan_1->setText("Channel 1");
             chan_1->setChecked(true);
             actual_channel = 0;
             chan_1->setFont(font);           
-            chan_2 = new QRadioButton(main);
+            chan_2 = new QRadioButton(this);
             chan_2->resize(100,50);
             chan_2->setText("Channel 2");
             chan_2->setFont(font);
@@ -183,7 +202,7 @@ class GUI: public QMainWindow{
                 chan_2->setDisabled(false);
             }
             else chan_2->setDisabled(true);
-            chan_3 = new QRadioButton(main);
+            chan_3 = new QRadioButton(this);
             chan_3->resize(100,50);
             chan_3->setText("Channel 3");
             chan_3->setFont(font);
@@ -200,47 +219,47 @@ class GUI: public QMainWindow{
             channels->setGeometry(40,120,200,200);
 
             //flash and home buttons
-            flash_button = new QPushButton(main);
+            flash_button = new QPushButton(this);
             flash_button->setGeometry(40,350,100,50);
             flash_button->setText("Flash LED");
-            flash_button->addAction(flash_action);
-            flash_action = new QAction(main);
+            flash_action = new QAction(this);
             connect(flash_action, &QAction::triggered, this, &GUI::flash);
-            home_button = new QPushButton(main);
+            flash_button->addAction(flash_action);
+            home_button = new QPushButton(this);
             home_button->setGeometry(150,350,100,50);
             home_button->setText("Home");
-            home_button->addAction(home_action);
-            home_action = new QAction(main);
+            home_action = new QAction(this);
             connect(home_action, &QAction::triggered, this, &GUI::home);
+            home_button->addAction(home_action);
             
             //moves directional
-            moves_l = new QLabel(main);
+            moves_l = new QLabel(this);
             moves_l->setText("Moves");
             moves_l->setGeometry(475,30,100,50);
             font.setPointSize(15);
             moves_l->setFont(font);
-            forward = new QPushButton(main);
+            forward = new QPushButton(this);
             forward->setIcon(QIcon("./src/triangle_up.png"));
             forward->setIconSize(QSize(65,65));
             forward->setGeometry(400,100,70,70);
-            forward->addAction(forward_action);
-            forward_action = new QAction(main);
+            forward_action = new QAction(this);
             connect(forward_action, &QAction::triggered, this, &GUI::startD1);
-            backward = new QPushButton(main);
+            forward->addAction(forward_action);
+            backward = new QPushButton(this);
             backward->setIcon(QIcon("./src/triangle_down.png"));
             backward->setIconSize(QSize(65,65));
             backward->setGeometry(400,180,70,70);
-            backward->addAction(backward_action);
-            backward_action = new QAction(main);
+            backward_action = new QAction(this);
             connect(backward_action, &QAction::triggered, this, &GUI::startD2);
+            backward->addAction(backward_action);
             
-            dir_moves = new QGroupBox("Directional",main);
+            dir_moves = new QGroupBox("Directional",this);
             ldir_moves = new QVBoxLayout();
             font.setPointSize(12);
             dir_moves->setFont(font);
-            jogm = new QRadioButton("Jog",main);
+            jogm = new QRadioButton("Jog",this);
             jogm->resize(100,50);
-            velm = new QRadioButton("Velocity",main);
+            velm = new QRadioButton("Velocity",this);
             velm->resize(100,50);
             ldir_moves->addWidget(jogm);
             ldir_moves->addWidget(velm);
@@ -249,29 +268,29 @@ class GUI: public QMainWindow{
             dir_moves->setGeometry(500,80,150,175);
             
             //moves non-directional
-            moves = new QGroupBox(main);
+            moves = new QGroupBox(this);
             lmoves = new QGridLayout();
-            absm = new QRadioButton("Absolute",main);
+            absm = new QRadioButton("Absolute",this);
             absm->resize(100, 50);
-            abspl = new QLabel("Position",main);
+            abspl = new QLabel("Position",this);
             abspl->resize(50,50);
-            abspos = new QLineEdit(main);
+            abspos = new QLineEdit(this);
             abspos->setInputMask("999999999");
             abspos->setText("0");
             abspos->resize(80,25);
-            reldist = new QLineEdit(main);
+            reldist = new QLineEdit(this);
             reldist->setText("0");
             reldist->setInputMask("999999999");
             reldist->resize(80,25);
-            relm = new QRadioButton("Relative",main);
+            relm = new QRadioButton("Relative",this);
             relm->resize(100, 50);
-            reldl = new QLabel("Distance",main);
+            reldl = new QLabel("Distance",this);
             reldl->resize(50,50);
-            start = new QPushButton("Start",main);
+            start = new QPushButton("Start",this);
             start->resize(50,40);
-            start->addAction(start_action);
-            start_action = new QAction(main);
+            start_action = new QAction(this);
             connect(start_action, &QAction::triggered, this, &GUI::startm);
+            start->addAction(start_action);
             lmoves->setVerticalSpacing(4);
             lmoves->addWidget(absm,0,0);
             lmoves->addWidget(abspos,0,2);
@@ -284,14 +303,14 @@ class GUI: public QMainWindow{
             moves->setGeometry(400,250,300,200);
             
             //stop button
-            stop = new QPushButton("Stop",main);
+            stop = new QPushButton("Stop",this);
             stop->setGeometry(275,380,100,50);
-            stop->addAction(stop_action);
-            stop_action = new QAction(main);
+            stop_action = new QAction(this);
             connect(stop_action, &QAction::triggered, this, &GUI::stopm);
+            stop->addAction(stop_action);      
             
             //status bar
-            status_box = new QGroupBox("Status",main);
+            status_box = new QGroupBox("Status",this);
             lstat = new QGridLayout();
             font.setPointSize(11);
             status_box->setFont(font);
@@ -302,7 +321,7 @@ class GUI: public QMainWindow{
             pos.append(std::to_string(opened_device.motor[actual_channel].status_position));
             std::string encc = "Encoder counter: ";
             std::string vel = "Velocity: ";
-            position = new QLabel(pos.c_str(),main);
+            position = new QLabel(pos.c_str(),this);
             if (opened_device.enc_counter == 1) {
                 encc.append(std::to_string(opened_device.motor[actual_channel].status_enc_count));
                 vel.append("Unknown");
@@ -311,8 +330,8 @@ class GUI: public QMainWindow{
                 vel.append(std::to_string(opened_device.motor[actual_channel].status_velocity));
                 encc.append("Unknown");
             }
-            enc_count = new QLabel(encc.c_str(),main);
-            velocity = new QLabel(vel.c_str(),main);
+            enc_count = new QLabel(encc.c_str(),this);
+            velocity = new QLabel(vel.c_str(),this);
             
             homing->setEnabled(opened_device.motor[actual_channel].homing);
             moving->setEnabled(opened_device.motor[actual_channel].moving);
@@ -327,7 +346,7 @@ class GUI: public QMainWindow{
             status_box->setGeometry(10,450,780,140);
             
             
-            main->show();
+            this->show();
         }
         
         
